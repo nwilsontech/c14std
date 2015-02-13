9392c0b4071377624c0515409001d29fc0c98184
nwilsontech <nathaniel.wilson6@mycampus.apus.edu>
2015-02-13 14:41:10 -0500

    Update Minor

--- poly/main.cpp
+++ poly/main.cpp
@@ -22,6 +22,9 @@ freely, subject to the following restrictions:
 #include <vector>
 #include <string>
 
+#if __cplusplus < 201103L
+    #error "Make Sure To Compile with C++11 or greater"
+#endif
 ///Scope In entire STD namespace
 using namespace std;
 
@@ -34,10 +37,13 @@ using i64 = long long int;
 void f(long) { cout << "f(long)\n"; }
 void f(char *) { cout << "f(char *)\n";}
 
+
+
 void Moo()
 {
     cout << __PRETTY_FUNCTION__ << endl;
 }
+
 
 std::string format_str_w(std::string s,std::string notation)
 {
@@ -53,8 +59,65 @@ std::string format_str_w(std::string s,std::string notation)
     return ret; /// finally return
 }
 
+template<int N>
+struct absV1 {
+    static_assert( N != INT_MIN,"Not Int" );
+    static constexpr int value = (N < 0) ? - N: N;
+};
+
+/// template meta functions.
+
+template<unsigned M, unsigned N>
+struct gcd{
+    static int const value = gcd<N,M%N>::value;
+};
+template<unsigned M>
+struct gcd<M,0>{
+    static_assert( M != 0u," Value is not Zero");
+    static int const value = M;
+};
+/*
+int fib(int x) {
+    if (x == 1) {
+        return 1;
+    } else {
+        return fib(x-1)+fib(x-2);
+    }
+}
+ */
+
+//template<int F>
+//struct fib{
+//    static constexpr int value = fib<F-1>::value + fib<F-2>::value;
+//};
+//template<int G>
+//struct fib<G>{
+//    static_assert( G != 1, "Failed");
+//    static constexpr int value = G;
+//};
+
+//template<int U>
+//struct fib<U>{
+//    static_assert( U == 1, "Failed");
+//    static constexpr int value = 1;
+//};
+//template<typename _Tp>
+//  using fib_v = typename fib<_Tp>::value;
+
+//template<class T>
+//struct remove_const {
+//    using type = T;
+//};
+
+
+
 int main()
 {
+    const int ret = 1;
+    //const int nu = fib<30>::value;
+    //cout << nu << endl;
+    cout <<gcd<982u,125u>::value<<endl;
+    cout << absV1<ret>::value << endl;
     cout << format_str_w("1000000000",",") << endl;
     cout << format_str_w("100000000",",") << endl;
     cout << format_str_w("10000000",",") << endl;
